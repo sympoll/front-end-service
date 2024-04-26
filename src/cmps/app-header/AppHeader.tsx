0c3e9e668bb02415857767d0c642441fb0907c65
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-04-26 06:15:18 +0300

    added link to the header title

--- src/cmps/app-header/AppHeader.tsx
+++ src/cmps/app-header/AppHeader.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function AppHeader() {
-  return <div className="app-header-container">Sympoll™</div>;
+  const navigate = useNavigate();
+
+  // Can change later the navigation to navigate to the feed page.
+  // For now more convenient to navigate to the landing page.
+  return (
+    <div className="app-header-container" onClick={() => navigate("/")}>
+      <div id="app-header-title">Sympoll™</div>
+    </div>
+  );
 }

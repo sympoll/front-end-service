363043b051c916968e565b2600bde5ad90d6a408
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-04-29 18:57:53 +0300

    fixed more issues...

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,11 @@ export default function App() {
     // can add silent login here to auto try to sign in, and if not previously signed in within a certain time, auto redirect to the landing page
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element="LandingPage" />
-        <Route path="/login" element="LogInPage" />
-        <Route path="/feed" element="FeedPage" />
-        <Route path="/feeds" element="FeedPage" />
+        <Route path="/*" element={<LandingPage />} />
+        <Route path="/login" element={<LogInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/feed" element={<FeedPage />} />
+        <Route path="/feed/*" element={<FeedPage />} />
       </Routes>
     </BrowserRouter>
   );

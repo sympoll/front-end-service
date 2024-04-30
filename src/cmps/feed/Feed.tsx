5656b8bf31847e96aee3c9d8cc649a46048b00b0
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-04-30 12:34:33 +0300

    Added sections to Poll component, added props to pass data into the component

--- src/cmps/feed/Feed.tsx
+++ src/cmps/feed/Feed.tsx
@@ -4,9 +4,18 @@ import Poll from "./Poll";
 export default function Feed() {
   return (
     <div className="feed-container">
-      <Poll title="Poll 1" />
-      <Poll title="Poll 2" />
-      <Poll title="Poll 3" />
+      <Poll
+        title="Poll Title 1"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam convallis vehicula placerat."
+      />
+      <Poll
+        title="Poll Title 2"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam convallis vehicula placerat. Nunc bibendum mauris ac sollicitudin commodo. Maecenas ullamcorper."
+      />
+      <Poll
+        title="Poll Title 3"
+        content="Nam convallis vehicula placerat. Nunc bibendum mauris ac sollicitudin commodo. Maecenas ullamcorper."
+      />
     </div>
   );
 }
--- src/cmps/feed/Poll.tsx
+++ src/cmps/feed/Poll.tsx
@@ -2,13 +2,16 @@ import React from "react";
 
 interface PollProps {
   title: string;
+  content: string;
 }
 
-export default function Poll({ title }: PollProps) {
+export default function Poll({ title, content }: PollProps) {
+  // Convert the voting container into a component
   return (
     <section className="poll-item">
       <div id="poll-item-title">{title}</div>
-      <div id="poll-item-content"></div>
+      <div id="poll-item-content">{content}</div>
+      <div id="poll-item-voting-container">Voting stuff</div>
     </section>
   );
 }

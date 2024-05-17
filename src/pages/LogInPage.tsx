0a130f99c7cf48b1b36379e2b195b15a45d0ec6d
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-05-17 16:49:52 +0300

    created login page frame

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import Button from "../cmps/global/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function LogInPage() {
+  const navigate = useNavigate();
+
   return (
     <section className="login-page-container">
-      <div>Log-in Page</div>
+      <p className="login-page-title">Login to Sympoll™</p>
+      <div className="login-page-buttons">
+        <Button onClick={() => navigate("/login")}>Log In</Button>
+        <Button onClick={() => navigate("/")}>Go Back</Button>
+      </div>
     </section>
   );
 }

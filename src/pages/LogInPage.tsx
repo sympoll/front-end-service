9ad5a50fe7c6d0dc17529b5d35a5bef13badb4d5
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-05-17 18:51:05 +0300

    finished log in and sign up pages. without the backend logic.

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -2,15 +2,25 @@ import React, { useState } from "react";
 import Button from "../cmps/global/Button";
 import { Link, useNavigate } from "react-router-dom";
 
+const SPACE = " ";
+const EMPTY_STR = "";
+const MAX_PASS_LEN = 20;
+const MAX_USERNAME_LEN = 20;
+const MAX_EMAIL_LEN = 20;
+
 export default function LogInPage() {
-  const [isSignIn, setIsSignIn] = useState(true); // true = sign in mode, false = sign up mode
+  const [isSignIn, setIsSignIn] = useState(true); // true = log in mode, false = sign up mode
+  const [username, setUsername] = useState(EMPTY_STR);
+  const [email, setEmail] = useState(EMPTY_STR);
+  const [password, setPassword] = useState(EMPTY_STR);
+  const [passwordConfirm, setPasswordConfirm] = useState(EMPTY_STR);
   const navigate = useNavigate();
 
   const handleModeToggle = () => {
     setIsSignIn(!isSignIn);
   };
 
-  const handleSignIn = (event: React.FormEvent) => {
+  const handleLogIn = (event: React.FormEvent) => {
     // Add login logic
   };
 
@@ -18,22 +28,70 @@ export default function LogInPage() {
     // Add sign up logic
   };
 
+  const preventSpaceKeyPress = (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (event.key === SPACE) event.preventDefault();
+  };
+
   return (
     <section className="login-page-container">
       <div className="login-form-container">
+        <p className="login-form-title">
+          {isSignIn ? "Log In" : "Sign Up"} to Sympoll™
+        </p>
         <form
           className="login-form"
-          onSubmit={isSignIn ? handleSignUp : handleSignIn}
+          onSubmit={isSignIn ? handleLogIn : handleSignUp}
         >
-          <p className="login-form-title">
-            {isSignIn ? "Sign Up" : "Log In"} to Sympoll™
-          </p>
-          <button
-            type="submit"
-            onClick={isSignIn ? handleSignUp : handleSignIn}
-          >
-            {isSignIn ? "Sign In" : "Sign Up"}
-          </button>
+          <input
+            type="text"
+            onKeyDown={preventSpaceKeyPress}
+            maxLength={MAX_USERNAME_LEN}
+            placeholder="Username..."
+            value={username}
+            onChange={(event: any) => {
+              if (event.nativeEvent.keyCode === 32) event.preventDefault();
+              else setUsername(event.target.value);
+            }}
+          />
+          {!isSignIn && (
+            <input
+              type="text"
+              onKeyDown={preventSpaceKeyPress}
+              maxLength={MAX_EMAIL_LEN}
+              placeholder="Email..."
+              value={email}
+              onChange={(event: any) => {
+                if (event.nativeEvent.keyCode === 32) event.preventDefault();
+                else setEmail(event.target.value);
+              }}
+            />
+          )}
+          <input
+            type="password"
+            onKeyDown={preventSpaceKeyPress}
+            maxLength={MAX_PASS_LEN}
+            placeholder="Password..."
+            value={password}
+            onChange={(event: any) => {
+              setPassword(event.target.value);
+            }}
+          />
+          {!isSignIn && (
+            <input
+              type="password"
+              onKeyDown={preventSpaceKeyPress}
+              maxLength={MAX_PASS_LEN}
+              placeholder="Confirm Password..."
+              value={passwordConfirm}
+              onChange={(event) => {
+                setPasswordConfirm(event.target.value);
+              }}
+            />
+          )}
+
+          <button type="submit">{isSignIn ? "Log In" : "Sign Up"}</button>
         </form>
         <div className="login-form-forgot-info">
           <p>
@@ -42,7 +100,7 @@ export default function LogInPage() {
           <p>
             {isSignIn ? "Don't have an account? " : "Already have an account? "}
             <Link to="" onClick={handleModeToggle}>
-              {isSignIn ? "Sign up" : "Sign in"}
+              {isSignIn ? "Sign Up" : "Log In"}
             </Link>
           </p>
         </div>

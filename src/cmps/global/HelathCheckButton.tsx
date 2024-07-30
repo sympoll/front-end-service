9ae65b4f41479d2ef888fce1e9399075f360987e
Ronenii <ronen.glm@gmail.com>
2024-07-30 10:59:42 +0300

    Added logging

--- src/cmps/global/HelathCheckButton.tsx
+++ src/cmps/global/HelathCheckButton.tsx
@@ -1,29 +1,33 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from "react";
+import logger from "../../logger/logger";
 
 interface HealthCheckResponse {
-    status: string;
-  }
-  
-  function HealthCheckButton() {
-    const [healthStatus, setHealthStatus] = useState<string | null>(null);
-  
-    const fetchHealthStatus = async () => {
-      try {
-        const response = await fetch('http://poll-manager:8081/api/poll/health');
-        const data: HealthCheckResponse = await response.json();
-        setHealthStatus(data.status);
-      } catch (error) {
-        console.error('Error fetching health status:', error);
-        setHealthStatus('Error fetching health status');
-      }
-    };
-  
-    return (
-      <div>
-        <button onClick={fetchHealthStatus}>Check Health</button>
-        <header>Health Status: {healthStatus || 'Not checked yet'}</header>
-      </div>
-    );
-  }
+  status: string;
+}
+
+function HealthCheckButton() {
+  const [healthStatus, setHealthStatus] = useState<string | null>(null);
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const fetchHealthStatus = async () => {
+    try {
+      logger.info("Try to fetch from URL: " + backendUrl + "/api/poll/health");
+      const response = await fetch(backendUrl + "/api/poll/health");
+      logger.info("Awaiting fetch from: " + backendUrl + "/api/poll/health");
+      const data: HealthCheckResponse = await response.json();
+      logger.info("Completed fetch from: " + backendUrl + "/api/poll/health");
+      setHealthStatus(data.status);
+    } catch (error) {
+      console.error("Error fetching health status:", error);
+      setHealthStatus("Error fetching health status");
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={fetchHealthStatus}>Check Health</button>
+      <header>Health Status: {healthStatus || "Not checked yet"}</header>
+    </div>
+  );
+}
 export default HealthCheckButton;

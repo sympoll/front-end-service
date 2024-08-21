5c58bd9520581cc19e9edce1337507c4e930d4de
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-08-21 16:32:17 +0300

    Added error and sigup services for the sign up process. added model for the user data defined in the user signup form.

---
+++ src/models/UserData.model.ts
@@ -0,0 +1,5 @@
+export interface UserData {
+    userName: string;
+    password: string;
+    email: string;
+}
---
+++ src/services/signup.service.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { UserData } from "../models/UserData.model";
+import { throwAxiosErr } from "./error.service";
+
+const userServiceUrl = 
+    import.meta.env.VITE_BASE_URL + 
+    import.meta.env.VITE_API_GATEWAY_URL + 
+    import.meta.env.VITE_USER_SERVICE_URL;
+
+
+    
+/**
+ * Send a request to user-service to signup the user.
+ * @param userData Information of the user, filled in the sign up form.
+ */
+export async function envokeSignUp(userData : UserData) {
+    try {
+        const response = await axios
+            .create({
+                baseURL: userServiceUrl,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                withCredentials: true
+            })
+            .post("", userData);
+
+        return response.data;
+    } catch (err) {
+        console.error("Error signing up the use:r " + userData + ".");
+        throw throwAxiosErr(err);
+    }
+}

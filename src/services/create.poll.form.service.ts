6f5f4a7d831c8a0fbcb8f3036f4a3971d4b9cbef
Ronen Gelmanovich <ronenge@mta.ac.il>
2024-08-25 21:35:31 +0300

    forgot to remove function, stupid me...

--- src/services/create.poll.form.service.ts
+++ src/services/create.poll.form.service.ts
@@ -61,19 +61,6 @@ export function isAllVotingItemsDefined(votingItems: string[]): boolean {
     };
 }
 
-function formatDateForInput(dateTime: string) {
-    // Convert to the format expected by datetime-local
-    isoDateTime = 
-    const date = new Date(dateTime);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-
-    return `${year}-${month}-${day}T${hours}:${minutes}`;
-};
-
 
 export async function handleSubmit(
     formData: CreatePollData,

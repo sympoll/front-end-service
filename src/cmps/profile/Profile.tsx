fe071a8cbf36d4a6ae059725396267315e213caf
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-08-25 14:19:22 +0300

    Added user profile display.

--- src/cmps/profile/Profile.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
-export default function Profile() {
-  const { username } = useParams();
-
-  return <div>username logged in {username}</div>;
-}

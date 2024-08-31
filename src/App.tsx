c540cf773d96e8e0d8a06725723ddc6a0a490d6c
Roy <roytol12@gmail.com>
2024-08-31 18:14:04 +0300

    Some changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,23 @@ import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import ContentPage from "./pages/ContentPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import { GroupsProvider } from "./cmps/group/GroupContext";
 
 export default function App() {
   return (
     // can add silent login here to auto try to sign in, and if not previously signed in within a certain time, auto redirect to the landing page
     // TODO: add reset accound page
     <BrowserRouter>
-      <GroupsProvider>
-        <Routes>
-          <Route path="/*" element={<NotFoundPage />} />
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/reset-account" element={<NotFoundPage />} />
+      <Routes>
+        <Route path="/*" element={<NotFoundPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/reset-account" element={<NotFoundPage />} />
 
-          <Route path="/feed/*" element={<ContentPage content="feed" />} />
-          <Route path="/feed/:groupId" element={<ContentPage content="feed" />} />
-          <Route path="/:userId" element={<ContentPage content="user-profile" />} />
-          <Route path="/group/:groupId" element={<ContentPage content="group-info" />} />
-        </Routes>
-      </GroupsProvider>
+        <Route path="/feed/*" element={<ContentPage content="feed" />} />
+        <Route path="/feed/:groupId" element={<ContentPage content="feed" />} />
+        <Route path="/:userId" element={<ContentPage content="user-profile" />} />
+        <Route path="/group/:groupId" element={<ContentPage content="group-info" />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { GroupsProvider } from "./cmps/group/GroupContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <GroupsProvider>
+      <App />
+    </GroupsProvider>
   </React.StrictMode>
 );
+
+
+

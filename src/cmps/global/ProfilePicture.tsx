dae3a627f1d88904135c656ea5b867402f3c4956
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-09-02 22:45:25 +0300

    Now displaying user info on the groups sidebar. Also added onClick function to the profile picture component, which is a optional prop that allows for functionallity like on click move to user profile page.

--- src/cmps/global/ProfilePicture.tsx
+++ src/cmps/global/ProfilePicture.tsx
@@ -2,17 +2,27 @@ import React from "react";
 
 interface ProfilePictureProps {
   imageUrl: string;
+  onClick?: () => void;
   size?: string;
   altText?: string;
 }
 
 export default function ProfilePicture({
   imageUrl,
+  onClick,
   size = "40px",
   altText = "Profile Picture"
 }: ProfilePictureProps) {
   return (
-    <div className="profile-picture-container" style={{ width: size, height: size }}>
+    <div
+      className="profile-picture-container"
+      onClick={onClick}
+      style={{
+        width: size,
+        height: size,
+        cursor: onClick ? "pointer" : "default"
+      }}
+    >
       <img className="profile-picture-image" src={imageUrl} alt={altText}></img>
     </div>
   );

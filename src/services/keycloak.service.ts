f8bfdabb6af7b16096339465bb24997260cf8463
Ronen Gelmanovich <ronenge@mta.ac.il>
2024-09-05 19:16:14 +0300

    Added environment variables

--- src/services/keycloak.service.ts
+++ src/services/keycloak.service.ts
@@ -2,9 +2,9 @@ import Keycloak, { KeycloakConfig } from "keycloak-js";
 
 
 const keycloakConfig: KeycloakConfig = {
-    url: import.meta.env.VITE_BASE_AUTH_URL,
-    realm: "sympoll-realm",
-    clientId: 'frontend-client'
+    url: import.meta.env.VITE_KEYCLOAK_URL,
+    realm: import.meta.env.VITE_KEYCLOAK_REALM,
+    clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID
 }
 
 const keycloak = new Keycloak(keycloakConfig);

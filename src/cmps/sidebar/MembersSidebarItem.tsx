be2f22dba5b28b06714c283954dc023b51b4d69c
Roy <roytol12@gmail.com>
2024-09-08 17:09:45 +0300

    fixed profile picture in memeber item

--- src/cmps/sidebar/MembersSidebarItem.tsx
+++ src/cmps/sidebar/MembersSidebarItem.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ProfilePicture from "../global/ProfilePicture";
 import defaultProfilePictureUrl from "/imgs/profile/blank-profile-picture.jpg";
+import { fetchPicture } from "../../services/media.service";
 
 interface MembersSidebarItemProps {
   name: string;
@@ -20,6 +21,7 @@ export default function MembersSidebarItem({
   // If currently in the given path, set the sidebar item's ID as active.
   const navigate = useNavigate();
   const [isRegularMember, setIsRegularMember] = useState(true);
+  const [profileImageSrc, setProfileImageSrc] = useState<string>(defaultProfilePictureUrl);
 
   useEffect(() => {
     if (role !== "Member") {
@@ -29,12 +31,23 @@ export default function MembersSidebarItem({
     }
   }, [role]);
 
+  useEffect(() => {
+    fetchPicture(profilePictureUrl)
+    .then((data) => {
+      console.log("Fetched user's profile picture");
+      setProfileImageSrc(data ?? defaultProfilePictureUrl);
+    })
+    .catch((error) => {
+      console.log("Unable to fetch user's profile picture");
+    });
+  }, [profileImageSrc]);
+
   return (
     <li className="members-sidebar-item-container" onClick={() => navigate(path)}>
       <div id="members-sidebar-item-icon">
         <ProfilePicture
           size="20px"
-          imageUrl={profilePictureUrl ? profilePictureUrl : defaultProfilePictureUrl}
+          imageUrl={profileImageSrc}
         />
       </div>
       <div id="members-sidebar-item-name">{name}</div>
